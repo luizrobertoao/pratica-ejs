3e2a26851b9e28a6ddc094ac2ec345adf12918d4
luizrobertoao <luizrobertoao29@gmail.com>
2020-03-27 18:41:25 -0300

    Estudos de template engine com <%ejs.

--- app.js
+++ app.js
@@ -3,7 +3,9 @@ const rotas = require('./src/routes');
 const app = express();
 
 
-app.use(express.static(__dirname + '/public'))
+app.use(express.static('public'))
+app.set('view engine', 'ejs')
+app.set('views', './src/views');
 app.use(rotas)
 app.use((req, res)=>{
     return res.send("Acho que você se perdeu!");
